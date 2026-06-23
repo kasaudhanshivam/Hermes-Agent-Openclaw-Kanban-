02b9913dc811976bab6df88b474d225cd9140da2
Shivam Kasaudhan <kasaudhanshivam@users.noreply.github.com>
2026-06-23 15:00:40 +0000

    Fix: Add missing Board model import in CardController

--- kanban-api/app/Http/Controllers/Api/CardController.php
+++ kanban-api/app/Http/Controllers/Api/CardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CardResource;
+use App\Models\Board;
 use App\Models\BoardList;
 use App\Models\Card;
 use Illuminate\Http\Request;
